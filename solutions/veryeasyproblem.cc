4a5c5e439eb5fb3d12b32683611567b7b829af1d
Peanut Scuffy <124562280+P55D2K@users.noreply.github.com>
2023-04-18 11:57:41 +0800

    veryeasyproblem

---
+++ solutions/veryeasyproblem.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n; cin >> n;
+    cout << n << endl;
+
+    // while get_ultrasound() != 0:
+    //     print(get_ultrasound())
+    // go(20, 20, 0.3)
+
+    return 0;
+}
