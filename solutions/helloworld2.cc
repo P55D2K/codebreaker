c704fbcbc303f95ab13fc7bc92ff787a2e44fada
Peanut Scuffy <124562280+P55D2K@users.noreply.github.com>
2023-04-19 16:56:57 +0800

    helloworld2

---
+++ solutions/helloworld2.cc
@@ -0,0 +1,16 @@
+// there is no submit button
+// go to the html of another page and copy the submit button
+// paste it in the html of this page to submit
+// use this code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "Hello World";
+
+    return 0;
+}
