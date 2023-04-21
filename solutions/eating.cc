66b4b9ae2bc5f4373834d9bd9d1541b382c7b7e8
Peanut Scuffy <124562280+P55D2K@users.noreply.github.com>
2023-04-21 12:36:52 +0800

    eating

--- solutions/eating.cc
+++ solutions/eating.cc
@@ -1,4 +1,5 @@
 // this code gets onl 95/100 i cant get past tle in subtask 6 :(
+// and im too lazy to use xblue707's solution so :/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,26 +13,22 @@ int main() {
 
     ll ops; cin >> ops;
     ld final_output[ops];
-    map<ld, set<ld>> person_brand;
+    unordered_map<ld, set<ld>> person_brand;
     ld tmp_person, tmp_brand;
 
     for (ll i = 0; i < ops; i++) {
         cin >> tmp_person >> tmp_brand;
         auto it = person_brand[tmp_person].find(tmp_brand);
-        if (it != person_brand[tmp_person].end()) {
-            final_output[i] = 0;
-        } else {
+        if (it != person_brand[tmp_person].end()) { final_output[i] = 0; }
+        else {
             person_brand[tmp_person].insert(tmp_brand);
             final_output[i] = 1;
         }
     }
 
     for (ll i = 0; i < ops; i++) {
-        if (final_output[i]) {
-            cout << "YES\n";
-        } else {
-            cout << "NO\n";
-        }
+        if (final_output[i]) { cout << "YES\n"; }
+        else { cout << "NO\n"; }
     }
     return 0;
 }
