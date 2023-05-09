6cd6f7f16519bc6311c7f91de90a411eeb93d056
ZK <231637w@student.hci.edu.sg>
2023-05-09 10:43:18 +0800

    sts

--- solutions/subtaskstiching.py
+++
@@ -1,9 +0,0 @@
-# this is not best solution, it depends on luck
-# just keep submitting until you get 100 points
-# mine onlu worked after 16 submissions
-
-import random
-if random.randint(0, 1) == 0:
-    print(1)
-else:
-	print(2)
