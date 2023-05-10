bfe4789921d82bd688c180c2af6578087a19f761
ZK <231637w@student.hci.edu.sg>
2023-05-10 09:23:27 +0800

    unreadble

--- solutions/1821.cc
+++ solutions/1821.cc
@@ -1,9 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout << 0 << ' ' << 0;
-    return 0;
-}
+#include <iostream>
+using namespace std;int main(){cout<<0<<" "<<0;}
